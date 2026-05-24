bfca4407b5690cdd91da1c5884561e087b546c2e
ajay-QA76 <ajay.testing.work@gmail.com>
2026-05-24 17:00:46 +0530

    Update LoginTest.java
    Added LoginTest class

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,1 +1,18 @@
+package com.automation.tests.login;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest() {
+
+        String expected = "Products";
+        String actual = "Products";
+
+        Assert.assertEquals(actual, expected);
+
+        System.out.println("Login Test Passed");
+    }
+}
